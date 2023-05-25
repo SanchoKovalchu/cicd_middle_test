7793fd4d0f35b3513a164cf3584111add265d235
okovwork <okovwork@gmail.com>
2023-05-25 09:34:29 +0300

    new migrations, new recipes and categories

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Recipe
 
+def gallery_view(request):
+    return render(request, 'gallery.html', {
+        'categories': Category.objects.all(),
+    })
+
+def image_detail(request, pk):
+    image = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'image_detail.html', {
+        'image': image
+    })
